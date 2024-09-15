c7422bc57720e0543563fb246e689ace52329df7
Hernan Lopez <her.lopezs@duocuc.cl>
2024-09-15 14:24:40 -0300

    Se hacen responsivas las cards y se agregan más comics

--- src/app/biblioteca/biblioteca.page.ts
+++ src/app/biblioteca/biblioteca.page.ts
@@ -10,10 +10,13 @@ export class BibliotecaPage implements OnInit {
   cards = [
     {imageSrc: './../../assets/img/Daredevil1.jpg', title: 'Daredevil Vol5 #16', subtitle: '30 Páginas' },
     {imageSrc: './../../assets/img/Daredevil2.jpg', title: 'Daredevil Vol1 #28', subtitle: '36 Páginas' },
-    {imageSrc: './../../assets/img/Daredevil1.jpg', title: 'Daredevil Vol5 #16', subtitle: '30 Páginas' },
-    {imageSrc: './../../assets/img/Daredevil2.jpg', title: 'Daredevil Vol1 #28', subtitle: '36 Páginas' },
-    {imageSrc: './../../assets/img/Daredevil1.jpg', title: 'Daredevil Vol5 #16', subtitle: '30 Páginas' },
-    {imageSrc: './../../assets/img/Daredevil2.jpg', title: 'Daredevil Vol1 #28', subtitle: '36 Páginas' },
+    {imageSrc: './../../assets/img/Spiderman1.jpg', title: 'Amazing Fantasy #15', subtitle: '35 Páginas'},
+    {imageSrc: './../../assets/img/Ironman.jpg', title: 'Iron Man #300', subtitle: '30 Páginas'},
+    {imageSrc: './../../assets/img/BatmanAnnoUno.jpg', title: 'Batman Año Uno', subtitle: '186 Páginas'},
+    {imageSrc: './../../assets/img/HomunculusVol1.jpg', title: 'Homunculus Vol1', subtitle: '225 Páginas'},
+    {imageSrc: './../../assets/img/BeckVol1.jpg', title: 'Beck Vol1', subtitle: '245 Páginas'},
+ 
+ 
   ]
 
   ngOnInit() {
