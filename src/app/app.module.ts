f88c17542415da88d0ddfa3aff7194f22a1b852f
Franciscaduoc <fra.pulgar@duocuc.cl>
2024-12-02 01:59:04 -0300

    cambios en appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TabBarModule } from './components/tab-bar/tab-bar.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, TabBarModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, TabBarModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
