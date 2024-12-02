d7b5fb59d385732694287bee87bb7ec663aa1f42
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2024-12-01 22:53:38 -0300

    merging

--- src/app/components/card-shop/card-shop.component.spec.ts
+++ src/app/components/card-shop/card-shop.component.spec.ts
@@ -2,13 +2,37 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { CardShopComponent } from './card-shop.component';
-<<<<<<< HEAD
 import { MarvelApiService } from 'src/app/services/marvel-api.service';
-=======
+
+describe('CardShopComponent', () => {
+  let component: CardShopComponent;
+  let fixture: ComponentFixture<CardShopComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CardShopComponent ],
+      imports: [IonicModule.forRoot()], 
+      providers:[
+        {provide:MarvelApiService, useValue:{}}
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CardShopComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { CardShopComponent } from './card-shop.component';
 import { of } from 'rxjs';
 
 let marvelApiServiceMock: any;
->>>>>>> cc4579f49c490e644e39b7a8a604d2dce902e4db
 
 describe('CardShopComponent', () => {
   let component: CardShopComponent;
@@ -32,10 +56,7 @@ describe('CardShopComponent', () => {
 
     TestBed.configureTestingModule({
       declarations: [ CardShopComponent ],
-      imports: [IonicModule.forRoot()], 
-      providers:[
-        {provide:MarvelApiService, useValue:{}}
-      ]
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CardShopComponent);
