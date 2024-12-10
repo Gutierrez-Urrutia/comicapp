e222dbffbbfdb8c58fe9930771d0ac2ea68b22c7
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2024-12-10 00:42:45 -0300

    intentando usar la camara del telefono

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent  implements OnInit {
+export class AvatarComponent {
+  public foto: any;
+  public fotoUrl: any;
 
   constructor() { }
 
-  ngOnInit() {}
+  async sacarFoto() {
+    try {
+      this.foto = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        //source: CameraSource.Camera
+      });
+
+      this.fotoUrl = this.foto.webPath;
+    } catch (error) {
+      console.error('Error al tomar la foto:', error);
+    }
+  }
+
+  
 
 }
